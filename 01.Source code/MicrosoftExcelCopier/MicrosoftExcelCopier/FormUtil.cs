5c401f16e0c41cbafff7779477f271571451775b
lv.long.tn <lv.long.tn@45e7f7b4-5fe7-77ba-fb49-9e6b14f6f9c7>
2013-04-22 23:52:46 +0000

    Relocate

---
+++ 01.Source code/MicrosoftExcelCopier/MicrosoftExcelCopier/FormUtil.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MicrosoftExcelCopier
+{
+    public class FormUtil
+    {
+        public static DialogResult ShowMessageBoxLocalize(IWin32Window owner, string text, string caption, MessageBoxButtons buttons, MessageBoxIcon icon)
+        {
+            // Localize messagebox button text
+            MessageBoxManager.OK = vi_VN.buttonOKText;
+            MessageBoxManager.Yes = vi_VN.buttonYesText;
+            MessageBoxManager.No = vi_VN.buttonNoText;
+            MessageBoxManager.Cancel = vi_VN.buttonCancelText;
+            MessageBoxManager.Register();
+
+            DialogResult result = MessageBox.Show(owner, text, caption, buttons, icon);
+
+            MessageBoxManager.Unregister();
+
+            return result;
+        }
+    }
+}
